f54e3d5e5e7b55aa574b79ea75c1353e94b9f2b3
Developer Alvision <developer@alvision.media>
2026-09-17 12:35:52 +0530

    Update Scene 2 with newly uploaded Scene 2 video

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import architecturalGateImg from './Images/PI SERVICES PICS/architectural gate.j
 
 // Landing page scene videos for mobile responsiveness
 import scene1Vid from './videos/Scene 1.mp4'
+import scene2Vid from './videos/Scene 2.mp4'
 import scene3Vid from './videos/Scene 3.mp4'
 import scene4Vid from './videos/Scene 4.mp4'
 
@@ -1240,7 +1241,7 @@ function App() {
             <SceneCanvas
               id="hall-scene"
               frameUrls={hallFrames}
-              videoUrl={scene3Vid}
+              videoUrl={scene2Vid}
               transition="slide-left"
               overlays={[]}
             />
